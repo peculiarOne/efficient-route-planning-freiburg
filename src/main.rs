ccf211d83a875dbf4c590e73e21c91f7c00813d8
peculiarOne <wayne@campbellscorner.org.uk>
2019-06-14 12:44:15 +0100

    got parsing xml to work

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use quick_xml::events::{BytesStart, Event};
 use quick_xml::Reader;
 use std::borrow::Cow;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     println!("Hello, world!");
-    read_from_osm_xml();
+    from_osm_xml();
 }
 
 type NodeId = u64;
@@ -25,24 +26,77 @@ struct Network {
 
 fn process_way() {}
 
-fn read_from_osm_xml() -> () {
+fn from_osm_xml() -> () {
+    let test_xml = r#"<osm>
+	<node>
+		<tag k="odbl" v="clean"/>
+	</node>
+        <way>
+            <nd>
+            </nd>
+        </way>
+    </osm>"#;
+
     let file = "data/rutland-latest.osm.xml";
     let xml_string = fs::read_to_string(file).expect("couldn't read osm file");
     let sample: String = xml_string.chars().into_iter().take(100).collect();
-    print!("read from osm file,\n {}", sample);
+    // print!("read from osm file,\n {}", sample);
 
+    // echo_xml(&xml_string);
+    // echo_xml(&test_xml);
+    process_osm_xml(&xml_string);
+}
+
+fn echo_xml(xml_string: &str) -> () {
     let mut reader = Reader::from_str(&xml_string);
 
     let mut buf = Vec::new();
-    let mut way_nodes: Vec<NodeId> = Vec::new();
+
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                println!("<{}>", String::from_utf8(e.name().to_vec()).unwrap())
+            }
+            Ok(Event::Empty(ref e)) => {
+                println!("<{}/>", String::from_utf8(e.name().to_vec()).unwrap())
+            }
+            Ok(Event::End(ref e)) => {
+                println!("</{}>", String::from_utf8(e.name().to_vec()).unwrap())
+            }
+            Ok(Event::Eof) => break,
+            _ => (),
+        }
+        buf.clear();
+    }
+}
+
+fn process_osm_xml(xml_string: &str) -> () {
+    let mut reader = Reader::from_str(&xml_string);
+
+    let mut buf = Vec::new();
+    let mut way_nodes: HashSet<NodeId> = HashSet::new();
     let mut in_way = false;
     let mut way_is_highway = false;
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(e)) => {
+            Ok(Event::Start(ref e)) => {
+                println!(
+                    "start of tag {}, in_way={}",
+                    String::from_utf8(e.name().to_vec()).unwrap(),
+                    in_way
+                );
                 match e.name() {
-                    b"way" => in_way = true,
+                    b"way" => {
+                        println!("<way> found");
+                        in_way = true
+                    }
+                    _ => (),
+                }
+            }
+            Ok(Event::Empty(ref e)) => {
+                match e.name() {
                     b"nd" if in_way => {
+                        println!("<nd> found in way");
                         // TODO attributes() returns an iterator. need to find the "ref" attribute
                         let node_ref = e.attributes().next().unwrap();
                         let val: Option<NodeId> = match &node_ref {
@@ -51,30 +105,47 @@ fn read_from_osm_xml() -> () {
                                 value: v,
                             }) => {
                                 let s = String::from_utf8(v.to_vec()).unwrap();
-                                let id:u64 = s.parse().unwrap();
+                                let id: u64 = s.parse().unwrap();
                                 Some(id)
-                            },
+                            }
                             _ => None,
+                        };
+                        match val {
+                            Some(node_id) => way_nodes.insert(node_id),
+                            _ => false,
                         };
                         // print!("got attrbute {}", node_ref);
                     }
                     b"tag" if in_way => {
+                        println!("<tag> found in way");
                         way_is_highway |= is_highway(e);
                     }
                     _ => (),
                 }
             }
-            Ok(Event::End(ref e)) if e.name() == b"way" => {
-                if way_is_highway {};
-                in_way = false;
+            Ok(Event::End(ref e)) => {
+                println!(
+                    "start of tag {}",
+                    String::from_utf8(e.name().to_vec()).unwrap()
+                );
+                match e.name() {
+                    b"way" => {
+                        if way_is_highway {};
+                        in_way = false;
+                    }
+                    _ => (),
+                }
             }
             Ok(Event::Eof) => break,
             _ => (),
         }
+        buf.clear();
     }
+
+    print!("read {} highway nodes from network", way_nodes.len())
 }
 
-fn is_highway(tag: BytesStart) -> bool {
+fn is_highway(tag: &BytesStart) -> bool {
     let osm_key = "k".as_bytes();
     let osm_value = "v".as_bytes();
 
